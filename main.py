01ce69c1fa5be7e6c38b260b072cd430b70a0d14
hirogura <310876036+hirogura@users.noreply.github.com>
2026-08-29 00:52:37 +0000

    Fix servEX sidebar unresponsive due to stale app.js cache, bump to v.1.8.1

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ from fastapi.templating import Jinja2Templates
 
 IS_ROOT = os.getuid() == 0
 
-app = FastAPI(title="serv-UI", version="1.8.0")
+app = FastAPI(title="serv-UI", version="1.8.1")
 
 
 @app.middleware("http")
